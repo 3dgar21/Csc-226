bd33e9ba92d5fb74e3747c4f78a0065e4d27adc2
3dgar21 <delangeledgar34@gmail.com>
2025-02-19 18:56:34 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GH
+//
+//  Created by Edgar Del angel on 2/19/25.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+    cout<<"Hello World"<<endl;
+    
+    
+    return 0;
+}
